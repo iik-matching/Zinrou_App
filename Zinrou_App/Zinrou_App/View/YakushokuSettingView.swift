769b6059f40dd0b15dd87f107973bb3c86097902
onigiri-z <k.tennis218@icloud.com>
2022-02-26 16:48:04 +0900

    modify:プレイヤー追加して、役職確認画面に行くと落ちる問題を解決 add:人狼の場合はテキストを赤くする機能を追加

--- Zinrou_App/Zinrou_App/View/YakushokuSettingView.swift
+++ Zinrou_App/Zinrou_App/View/YakushokuSettingView.swift
@@ -7,19 +7,35 @@ struct YakushokuSettingView: View {
         if baseData.isShowYakushokuView {
             ZStack{
                 Color.brown.ignoresSafeArea()
-                    VStack{
-                        ForEach(0..<baseData.game.players.count, id: \.self) { index in
-                            HStack{
-                                Text(baseData.game.players[index].name)
+                VStack{
+                    ForEach(0..<baseData.game.players.count, id: \.self) { index in
+                        HStack{
+                            Text(baseData.game.players[index].name)
                                 .font(.largeTitle)
+                            //人狼を赤くする
+                            if(baseData.game.players[index].yakushoku!.name == YakushokuConst.ZINROU){
                                 Text(baseData.game.players[index].yakushoku!.name)
-                                .font(.largeTitle)
+                                    .font(.largeTitle)
+                                    .foregroundColor(.red)
+                            }else{
+                                Text(baseData.game.players[index].yakushoku!.name)
+                                    .font(.largeTitle)
                             }
+                            
+                        }
                     }.padding()
+                    
+                    Button(action: {
+                        withAnimation{
+                            baseData.isShowYakushokuView.toggle()
+                        }
+                    }){
+                        Text("前の画面へ").font(.largeTitle)
+                    }
                 }
             }
         }
-
+        
     }
 }
 
@@ -27,9 +43,20 @@ struct YakushokuSettingView_Previews: PreviewProvider {
     static var previews: some View {
         YakushokuSettingView()
             .environmentObject({ () -> BaseViewModel in
-                            let baseData = BaseViewModel()
+                let baseData = BaseViewModel()
                 baseData.isShowYakushokuView = true
-                            return baseData
-                        }())
+                
+                //４人の場合のデータを設定
+                var yakushoku_array : [String] = [YakushokuConst.SIMIN,
+                                                  YakushokuConst.ZINROU,
+                                                  YakushokuConst.URANAISI,
+                                                  YakushokuConst.KISHI]
+                yakushoku_array = yakushoku_array.shuffled()
+                for i in 0...baseData.game.players.count - 1 {
+                    baseData.game.players[i].yakushoku = baseData.getYakushoku(yakushokuName: yakushoku_array[i])
+                }
+                
+                return baseData
+            }())
     }
 }
