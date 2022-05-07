65f913dd0df5174d7179827df52cbee12b72e256
onigiri-z <k.tennis218@icloud.com>
2022-05-07 17:12:55 +0900

    modify: 本当にこの人でいいですか？　確認アラート追加

--- Zinrou_App/Zinrou_App/Model/Uranaishi.swift
+++ Zinrou_App/Zinrou_App/Model/Uranaishi.swift
@@ -7,6 +7,10 @@ struct Uranaishi:YakushokuProtocol{
     var zinnei: String = ZinneiConst.SIMIN
     //アクションテキスト
     var actionText: String = ActionTextConst.URANAISI
+    //夜のアクション確認メッセージ
+    var yoruActionMessage: String = YoruActionMessageConst.URANAISI
+    //アクションの説明文
+    var extentionMessage:String = ExtentionMessageConst.URANAISI
     
     func action1(name:String, delegate:SelectProtocol){
         delegate.select(name: name)
